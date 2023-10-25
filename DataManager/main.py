7bf43c07813fa3d6b35f6cfa8aa85136b9d9a80b
Benjamin Couet <nust660@gmail.com>
2023-10-25 09:46:57 +0200

    Clear useless files

--- DataManager/main.py
+++ DataManager/main.py
@@ -11,9 +11,7 @@ params = np.array([
     'CIS_InfoImportantes',
     'CIS_CIP_Dispo_Spec',
     'CIS_GENER_bdpm',
-    'CIS_HAS_ASMR_bdpm',
     'CIS_CPD_bdpm',
-    'CIS_HAS_SMR_bdpm',
     'HAS_LiensPageCT_bdpm'
 ])
 
@@ -23,19 +21,19 @@ def lecture_base(bd):
 
 
 date=[
-    ['data/CIS_InfoImportantes.txt',"data/CIS_HAS_ASMR_bdpm.txt","data/CIS_HAS_SMR_bdpm.txt"],
-    [[1,3],[3,4],[3,4]],
-    ["y-m-d","ymd","ymd"]
+    ['data/CIS_InfoImportantes.txt'],
+    [[1,3]],
+    ["y-m-d"]
 ]
 
 
 # INITIALISATION
 dataManager = DataManager(url, params)
 
-#files = dataManager.getFiles()
+files = dataManager.getFiles()
 
  # NETTOYAGE DES DONNEES
-def clear_data():
+def missing_value_count():
     tab_ms_values =""
     for i in params:
         table=lecture_base("data/"+i+".txt")
@@ -45,24 +43,18 @@ def clear_data():
     return tab_ms_values
 
 
-    
-dates_ASMR=[]
-dates_SMR=[]
-    
-for i in range(len(date[0])):
-    data=pd.read_csv(date[0][i], sep="\t", header=None, encoding="latin1").iloc[:,date[1][i][0]:date[1][i][1]]
-    
-    if date[2][i]=='y-m-d':
-        print("Colone 1  :   \n",data.iloc[:,0].str.split('-').apply(lambda x: f"{x[2]}/{x[1]}/{x[0]}"))
-        print("Colone 2  :   \n",data.iloc[:,1].str.split('-').apply(lambda x: f"{x[2]}/{x[1]}/{x[0]}"))
 
-    if date[2][i]=='ymd':
-        if date[0][i]== "data/CIS_HAS_ASMR_bdpm.txt":
-            print('ASMR :  \n',data.iloc[:,0].apply(lambda date_int: f"{date_int % 100:02d}/{date_int // 100 % 100:02d}/{date_int // 10000}"))
+def clean_date():
+    for i in range(len(date[0])):
+        data=pd.read_csv(date[0][i], sep="\t", header=None, encoding="latin1").iloc[:,date[1][i][0]:date[1][i][1]]
         
-        if date[0][i]== "data/CIS_HAS_SMR_bdpm.txt":
-            print('SMR :  \n',data.iloc[:,0].apply(lambda date_int: f"{date_int % 100:02d}/{date_int // 100 % 100:02d}/{date_int // 10000}"))
+        if date[2][i]=='y-m-d':
+            print("Colone 1  :   \n",data.iloc[:,0].str.split('-').apply(lambda x: f"{x[2]}/{x[1]}/{x[0]}"))
+            print("Colone 2  :   \n",data.iloc[:,1].str.split('-').apply(lambda x: f"{x[2]}/{x[1]}/{x[0]}"))
         
+    
+
+print(lecture_base("data/CIS_CIP_bdpm.txt"))
 
 
 #Code CIS    Code CIP7   Libellé de la présentation  Statut administratif de la présentk,jation Etat de commercialisation
