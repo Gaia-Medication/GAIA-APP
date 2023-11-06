58aceccec80e5da5c0a48b95cb2aff695ee7e494
helldeal <alexandreclenet@free.fr>
2023-11-06 16:20:44 +0100

    up

--- App/app/(tabs)/_layout.tsx
+++ App/app/(tabs)/_layout.tsx
@@ -1,61 +1,79 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
-
-import Colors from '../../constants/Colors';
-import { Text, View } from '../../components/Themed';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable, useColorScheme } from "react-native";
+import { StyleSheet } from 'react-native';
+import Colors from "../../constants/Colors";
+import { Text, View } from "../../components/Themed";
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return <FontAwesome size={24} {...props} />;
 }
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
+  const styles = StyleSheet.create({
+    tabtitle: {
+      fontSize: 16,
+      fontWeight: '600',
+      marginLeft:16,
+      color: Colors[colorScheme ?? "light"].tint ,
+    },
+  });
+
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarStyle:{paddingVertical: 5,position:'absolute',height:50,borderTopWidth: 0,justifyContent:'space-between'},
+        tabBarLabelPosition: "beside-icon",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          tabBarLabel: ({ focused })=>focused?<Text>Accueil</Text>:null,
-          headerShown: false,
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          tabBarLabel: ({ focused }) => 
+            focused ? <Text style={styles.tabtitle}>Accueil</Text> : null,
+          tabBarIcon: ({ color }) =>
+            <TabBarIcon name="home" color={color} />,
         }}
       />
       <Tabs.Screen
         name="suivis"
         options={{
-          tabBarLabel: ({ focused })=>focused?<Text>Suivis</Text>:null,
-          headerShown: false,
-          tabBarLabelPosition: 'beside-icon',
-          tabBarIcon: ({ color }) => <TabBarIcon name="book" color={color} />,
+          tabBarLabel: ({ focused }) => 
+            focused ? <Text style={styles.tabtitle}>Suivis</Text> : null,
+          tabBarIcon: ({ color }) => 
+            <TabBarIcon name="book" color={color} />,
         }}
       />
       <Tabs.Screen
         name="messagerie"
         options={{
-          tabBarLabel: ({ focused })=>focused?<Text>Messagerie</Text>:null,
-          headerShown: false,
-          tabBarIcon: ({ color }) => <TabBarIcon name="envelope" color={color} />,
+          tabBarLabel: ({ focused }) =>
+            focused ? <Text style={styles.tabtitle}>Messagerie</Text> : null,
+          tabBarIcon: ({ color }) => 
+            <TabBarIcon name="envelope" color={color} />
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
-          tabBarLabel: ({ focused })=>focused?<Text>Paramètres</Text>:null, 
-          headerShown: false,
-          tabBarIcon: ({ color }) => <TabBarIcon name="gear" color={color} />,
+          tabBarLabel: ({ focused }) =>
+            focused ? <Text style={styles.tabtitle}>Paramètres</Text> : null,
+          tabBarIcon: ({ color }) => 
+            <TabBarIcon name="gear" color={color} />,
         }}
       />
     </Tabs>
   );
 }
+
+
