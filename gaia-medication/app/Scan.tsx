17dc8d72feb07fe63329f855620bfb8ab6025184
E214194U <alexandreclenet@free.fr>
2023-11-24 00:54:46 +0100

    image pick

--- gaia-medication/app/Scan.tsx
+++ gaia-medication/app/Scan.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import TextRecognition from '@react-native-ml-kit/text-recognition';
+
+
 
 export default function Scan() {
+  const [image, setImage] = useState("");
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+      const res = await TextRecognition.recognize(result.assets[0].uri);
+
+      console.log('Recognized text:', res.text);
+    }
+  };
+
   return (
-    <SafeAreaView edges={['top']}>
-      <Text>Scan</Text>
-      <Button
-        title='Click Here'
-        onPress={() => alert('Button Clicked!')}
-      />
-    </SafeAreaView>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+    </View>
   );
 }
