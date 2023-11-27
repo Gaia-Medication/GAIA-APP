b27250f5ed8ed43d0c0acce8cbbbcc2274e58ca0
E214194U <alexandreclenet@free.fr>
2023-11-27 02:51:11 +0100

    profile up

--- gaia-medication/app/Profile/Profile.tsx
+++ gaia-medication/app/Profile/Profile.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import { useIsFocused } from '@react-navigation/native';
+import React, { useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Profile() {
+  const isFocused = useIsFocused();
+  useEffect(() => {
+    if(isFocused){ 
+      console.log("Nav on Profile Page")
+    }
+  },[isFocused]); 
   return (
     <View>
       <Text>Profile</Text>
-      <Button
-        title='Click Here'
-        onPress={() => alert('Button Clicked!')}
-      />
     </View>
   );
 }
