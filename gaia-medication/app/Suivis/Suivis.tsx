a263635848a60169e13d33a630c6f7e1207d33b2
florian <florian.tranpro@gmail.com>
2023-12-12 15:50:09 +0100

    upd: traitement

--- gaia-medication/app/Suivis/Suivis.tsx
+++ gaia-medication/app/Suivis/Suivis.tsx
@@ -1,20 +1,25 @@
-import { useIsFocused } from '@react-navigation/native';
-import React, { useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { useIsFocused } from "@react-navigation/native";
+import React, { useEffect } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import * as Icon from "react-native-feather";
+import { styles } from "../../style/style";
 
 export default function Suivis() {
   const isFocused = useIsFocused();
   useEffect(() => {
-    if(isFocused){ 
-      console.log("Nav on Suivis Page")
+    if (isFocused) {
+      console.log("Nav on Suivis Page");
     }
-  },[isFocused]); 
-  
+  }, [isFocused]);
+
   return (
-    <SafeAreaView edges={['top']}>
-      <Text>Suivis</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <View className=" flex border-1 p-5">
+        <TouchableOpacity className=" flex flex-row items-center gap-3 justify-end">
+          <Text className=" text-[#363636] text-lg"> Ajouté un traitement</Text>
+          <Icon.Plus color="#363636" width={35} height={35}/>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
-
