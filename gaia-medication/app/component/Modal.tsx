46227bb3748ea89b3de0eb3172f76478f7fbc44a
florian <florian.tranpro@gmail.com>
2023-12-21 11:19:08 +0100

    change : modal

--- gaia-medication/app/component/Modal.tsx
+++ gaia-medication/app/component/Modal.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import { Modal, View, TouchableOpacity, Text } from 'react-native';
+import React, { useState } from "react";
+import { Modal, View, TouchableOpacity, Text } from "react-native";
 
-const ModalComponent = ({ visible, onClose, children }) => {
+const ModalComponent = ({ visible, onClose, children, styleAdded }) => {
   return (
     <Modal
       visible={visible}
@@ -10,21 +10,21 @@ const ModalComponent = ({ visible, onClose, children }) => {
       onRequestClose={onClose}
     >
       <TouchableOpacity
-        style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(0, 0, 0, 0.5)",
+        }}
         activeOpacity={1}
         onPress={onClose}
       >
-        <View
-          style={{
-            backgroundColor: 'white',
-            borderRadius: 10,
-            padding: 20,
-            minWidth: 300,
-          }}
-        >
+        <View style={styleAdded}>
           {children}
           <TouchableOpacity onPress={onClose}>
-            <Text style={{ marginTop: 10, textAlign: 'center', color: 'blue' }}>Fermer</Text>
+            <Text style={{ marginTop: 10, textAlign: "center", color: "blue" }}>
+              Fermer
+            </Text>
           </TouchableOpacity>
         </View>
       </TouchableOpacity>
