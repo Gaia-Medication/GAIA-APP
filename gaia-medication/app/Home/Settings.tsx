6fb57e0cb36d6e28286fb5e72824a9fb505bba87
florian <florian.tranpro@gmail.com>
2024-01-10 21:42:13 +0100

    add: addPDF

--- gaia-medication/app/Home/Settings.tsx
+++ gaia-medication/app/Home/Settings.tsx
@@ -13,6 +13,10 @@ import data from "./../Suivis/treatment.json";
 import { getAllTreatments, getUserByID, readList } from "../../dao/Storage";
 import TutorialBubble from "../component/TutorialBubble";
 
+import * as Print from "expo-print";
+import { shareAsync } from "expo-sharing";
+import { getMedbyCIS } from "../../dao/Meds";
+
 export default function Settings({ navigation }) {
   const [treatments, setTreatments] = useState<Treatment[]>([]);
 
@@ -22,9 +26,78 @@ export default function Settings({ navigation }) {
     setTutoSettings(await AsyncStorage.getItem("TutoSettings"));
   };
 
+  function formaterDate(date) {
+    const heures = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const jour = date.getDate().toString().padStart(2, "0");
+    const mois = (date.getMonth() + 1).toString().padStart(2, "0");
+    const annee = date.getFullYear();
+
+    return `${heures}:${minutes} ${jour}/${mois}/${annee}`;
+  }
+
   const handleTuto = (isClicked) => {
     AsyncStorage.setItem("TutoSettings", "1");
     navigation.navigate("Home");
+  };
+
+  const genererInstructionsHtml = async (instructions, users) => {
+    const htmlParts = await Promise.all(
+      instructions.map(async (instruction) => {
+        return await Promise.all(
+          instruction.takes.map(async (take) => {
+            const user = users.find((user) => user.id === take.userId);
+            const medic = await getMedbyCIS(take.CIS);
+            const date = formaterDate(new Date(take.date));
+            const takenStatus = take.taken ? "pris" : "non pris";
+            const review = take.review ? take.review : "";
+            return `
+            <span style="width: 100%; height: 1px; background-color: black; display: block; margin: 10px 0;"></span>
+            <p>${user.firstname} ${user.lastname}</p>
+            <p>Médicament: ${medic.Name}</p>
+            <p>Status: ${takenStatus}</p>
+            <p>le ${date}</p>
+            <p>Commentaire:${review}</p>
+            `;
+          })
+        );
+      })
+    );
+    return htmlParts.flat().join("");
+  };
+
+  const pdf = async () => {
+    const treatments = await getAllTreatments();
+    if (treatments.length === 0) {
+      alert("Vous n'avez pas de traitement");
+      return;
+    }
+    const users = await readList("users");
+    const treatment = treatments[0];
+    const instructionsHtml = await genererInstructionsHtml(
+      treatment.instructions,
+      users
+    );
+
+    const html = `
+      <html>
+      <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+        </head>
+        <body style="text-align: center;">
+          <h1 style="font-size: 30px; font-weight: normal;">
+            Traitement ${treatment.name}
+          </h1>
+          <h2 style="font-size: 24px; font-weight: normal;">Description</h2>
+          <p>${treatment.description}</p>
+          <h2 style="font-size: 24px; font-weight: normal;">Ressenti</h2>
+          <div style="font-size:16px;>${instructionsHtml}</div>
+        </body>
+      </html>
+    `;
+    const { uri } = await Print.printToFileAsync({ html });
+    console.log("File has been saved to:", uri);
+    await shareAsync(uri, { UTI: ".pdf", mimeType: "application/pdf" });
   };
 
   const isFocused = useIsFocused();
@@ -112,6 +185,7 @@ export default function Settings({ navigation }) {
       <Button onPress={deleteTreatments} title="Supprimer traitements" />
       <Button onPress={createTreatmentTest1} title="TraitementTest 1" />
       <Button onPress={reset} title="reset" />
+      <Button onPress={pdf} title="pdf" />
       {treatments &&
         treatments.map((treatment) => {
           return (
