14409681f85f869d6e5755fee415c51846163c95
E214194U <alexandreclenet@free.fr>
2024-01-12 00:51:11 +0100

    settings fix

--- gaia-medication/app/Home/Settings.tsx
+++ gaia-medication/app/Home/Settings.tsx
@@ -171,62 +171,6 @@ export default function Settings({ navigation }) {
           }
         ></TutorialBubble>
       )}
-      <Text>Settings</Text>
-      <Button
-        title="CLEAR USERS DATA"
-        onPress={() => (
-          AsyncStorage.removeItem("users"), AsyncStorage.removeItem("stock")
-        )}
-      />
-      <Button
-        title="CLEAR STOCK DATA"
-        onPress={() => AsyncStorage.removeItem("stock")}
-      />
-      <Button
-        title="ADD PROFILE"
-        onPress={() => navigation.navigate("CreateProfile")}
-      ></Button>
-      <Button
-        title="MODIFY PROFILE"
-        onPress={() => navigation.navigate("ModifyProfile")}
-      ></Button>
-      <Button onPress={notificationForgot} title="Notification oubli" />
-      <Button onPress={showTreatments} title="Liste des traitements" />
-      <Button onPress={deleteTreatments} title="Supprimer traitements" />
-      <Button onPress={createTreatmentTest1} title="TraitementTest 1" />
-      <Button onPress={reset} title="reset" />
-      {treatments &&
-        treatments.map((treatment) => {
-          return (
-            <View key={treatment.name}>
-              <Text>{treatment.name}</Text>
-              <Text>{treatment.instructions.length}</Text>
-            </View>
-          );
-        })}
-      {!treatments ? (
-        <Text>PAS DE VARIABLE ASYNC TREATMENT</Text>
-      ) : treatments.length == 0 ? (
-        <Text>TREATMENTS VIDE</Text>
-      ) : null}
-      <FlatList
-        data={settingsData}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => handleItemClick(item.id)} style={{ padding: 18, display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-            <Text style={{ color: "#333333", fontWeight: "700", fontSize: 18 }}>{item.title}</Text>
-            <Icon.ChevronRight color="#363636" width={23} height={23} />
-          </TouchableOpacity>
-        )}
-      />
-      <View style={{ display: "flex", flexDirection: "row", justifyContent: "center", marginVertical: 15 }}>
-        <View style={{ width: "90%", height: 1, backgroundColor: "#444444" }} />
-      </View>
-      <TouchableOpacity onPress={() => console.log("DELETE ACC")} style={{ padding: 18, display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
-        <Text style={{ color: "#FF0000", fontWeight: "700", fontSize: 18 }}>Delete Account</Text>
-        <Icon.Trash color="#FF0000" width={23} height={23} />
-      </TouchableOpacity>
-
 
       {debug && (
         <>
