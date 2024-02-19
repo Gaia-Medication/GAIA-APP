15f39f305d22ee28600bb09137082c3c036c1eef
Alexandre Clenet <alexandreclenet@free.fr>
2024-02-19 14:41:40 +0100

    test csv

--- gaia-medication/dao/MapPoint.ts
+++ gaia-medication/dao/MapPoint.ts
@@ -1,13 +1,35 @@
 import data from "./pharmacies.json";
+import Papa from 'papaparse';
 
 const points = JSON.parse(JSON.stringify(data));
+
+// async function GetData() {
+//   const data  = Papa.parse(await fetchCsv());
+//   console.log(data);
+//   return data;
+// }
+// GetData()
+
+// async function fetchCsv() {
+//   return fetch('. /pharmacies.csv').then(function (response) {
+//       let reader = response.body.getReader();
+//       let decoder = new TextDecoder('utf-8');
+//       return reader.read().then(function (result) {
+//           return decoder.decode(result.value);
+//       });
+//   }).catch(function(error) {
+//     console.log('There has been a problem with your fetch operation: ' + error.message);
+//      // ADD THIS THROW error
+//       throw error;
+//     });;
+// }
 
 //Renvoie le tableau de toutes les établissements avec coord & informations 
 export function getAllPoints() {
   try {
     return points;
   } catch (error) {
-    console.error("Error reading JSON file", error);
+    console.error("Error reading CSV file", error);
   }
 }
 
@@ -26,6 +48,6 @@ export function getPointsbyRegion(region) {
     points.sort((a, b) => a.distance - b.distance);
     return points.slice(0, 50);
   } catch (error) {
-    console.error("Error reading JSON file", error);
+    console.error("Error reading CSV file", error);
   }
 }
