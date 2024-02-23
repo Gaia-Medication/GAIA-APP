03d02dab7be2735e6b70e6a482c125755a44de79
Nuuust <nust660@gmail.com>
2024-02-23 16:41:00 +0100

    ATC

--- DataManager/therapeutic.py
+++ DataManager/therapeutic.py
@@ -1,5 +1,9 @@
 from dataManager import DataManager
-
+import pandas as pd
+from utils import lecture_base
+import json
+import os
+'''
 class Therapeutic():
     
     def getTherapeuticDatas(self, url, listOfCis):
@@ -18,3 +22,52 @@ class Therapeutic():
             except Exception as e:
                 print(f"An error occurred: {e} {listOfNames[i]} {listOfCis[i]}")
         return list_of_atc
+
+'''
+
+urlTherapeutic = 'https://base-donnees-publique.medicaments.gouv.fr/extrait.php?specid='
+urlAtc = "https://observatoiredumedicament.cyrilcoquilleau.com/medicament/"
+
+dfATC = pd.DataFrame(columns=["CIS","ATC","Indications_therapeutiques"])
+
+cis=lecture_base("data/CIS_bdpm.txt").iloc[:,0:1]
+name=lecture_base("data/CIS_bdpm.txt").iloc[:,1:2]
+
+
+file_path = 'out/atc.json'
+
+if not os.path.exists(file_path):
+    dfATC["CIS"]=cis
+    dfATC["name"]=name
+    atc_list = [None] * len(cis)
+    indications_therapeutiques_list = [None] * len(cis)
+    dfATC["ATC"] = atc_list
+    dfATC["Indications_therapeutiques"] = indications_therapeutiques_list
+    dfATC.to_json('out/atc.json', orient="records", indent=4)
+    
+    
+with open('out/atc.json', 'r') as file:
+    data = json.load(file)
+
+dfATC = pd.DataFrame(data)
+    
+dmTH=DataManager(urlTherapeutic)
+dmATC=DataManager(urlAtc)
+for row in data:
+    if  row.get("ATC") is None:
+        print("Null ATC", row["name"], row["CIS"])
+        cis = row.get("CIS")
+        r=dfATC[dfATC["CIS"]==cis]
+        i=r.index[0]
+        dfATC.loc[i,"ATC"]=dmATC.getATC(row["name"], row["CIS"])
+    if row.get("Indications_therapeutiques") is None:
+        print("Null therapeutic")
+        cis = row.get("CIS")
+        r=dfATC[dfATC["CIS"]==cis]
+        i=r.index[0]
+        dfATC.loc[i,"Indications_therapeutiques"]=dmTH.getTherapeutics(row["CIS"])
+        
+
+dfATC.to_json('out/atc.json', orient="records", indent=4)
+
+print("End of the process therapeutic.py")
